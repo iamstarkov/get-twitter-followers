641a792a7124fdbfaaaf12b798be1d66d810fdb6
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-10-16 21:13:32 +0200

    tests wip

--- test.js
+++ test.js
@@ -6,12 +6,11 @@ import tokens from 'twitter-tokens';
 it('should getTwitterFollowers', done => {
   getTwitterInfo(tokens, 'jsunderhood', (err, info) => {
     if (err) throw err;
-    console.log(info.followers_count)
     getTwitterFollowers(tokens, 'largescalejs_ru', (err, followers) => {
       if (err) throw err;
+      console.log(followers.next_cursor_str);
       equal(followers.length, info.followers_count);
       done();
     });
-
   })
 });
