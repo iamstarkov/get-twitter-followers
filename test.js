2d2361bb74de96757afcefcb47ccf7f84215b5b9
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-10-16 20:30:33 +0200

    write tests

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 import { equal } from 'assert';
 import getTwitterFollowers from './index';
+import getTwitterInfo from 'get-twitter-info';
+import tokens from 'twitter-tokens';
 
-it('should getTwitterFollowers', () =>
-  equal(getTwitterFollowers('unicorns'), 'unicorns'));
+it('should getTwitterFollowers', done => {
+  getTwitterInfo(tokens, 'jsunderhood', (err, info) => {
+    if (err) throw err;
+    console.log(info.followers_count)
+    getTwitterFollowers(tokens, 'largescalejs_ru', (err, followers) => {
+      if (err) throw err;
+      equal(followers.length, info.followers_count);
+      done();
+    });
 
-it('should getTwitterFollowers invalid input', () =>
-  equal(getTwitterFollowers(), undefined));
+  })
+});
