efd5f92084b64f2c244a1c3c158b6d5df226c3d5
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-10-16 21:14:59 +0200

    WIP

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-export default function getTwitterFollowers(input) {
-  return input;
+import Twitter from 'twit';
+import { merge, isEmpty, concat } from 'ramda';
+
+function accumulate(get, options, followers, cb) {
+  get(options, (err, res) => {
+    if (err) return cb(err);
+    if (isEmpty(res)) {
+      return cb(null, followers);
+    }
+    const accumulatedFollowers = concat(followers, res.users);
+    const nextOptions = merge(options, { cursor: res.next_cursor_str });
+    return accumulate(get, nextOptions, accumulatedFollowers, cb);
+  });
+}
+
+export default function getTwitterFollowers(tokens, username, cb) {
+  const client = new Twitter(tokens);
+  const get = client.get.bind(client, 'followers/list');
+  const options = { screen_name: username, count: 200 };
+  return accumulate(get, options, [], cb);
 };
