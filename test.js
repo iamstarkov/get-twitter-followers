49c739a007ee5aace495a0f3e269a38a9f5987c7
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-10-19 11:42:35 +0200

    refactor tests

--- test.js
+++ test.js
@@ -4,12 +4,11 @@ import getTwitterInfo from 'get-twitter-info';
 import tokens from 'twitter-tokens';
 
 it('should getTwitterFollowers', done => {
-  getTwitterInfo(tokens, 'jsunderhood', (err, info) => {
+  getTwitterInfo(tokens, 'jsunderhood', (err, { followers_count } = info) => {
     if (err) throw err;
     getTwitterFollowers(tokens, 'jsunderhood', (err, followers) => {
       if (err) throw err;
-      console.log(followers.next_cursor_str);
-      equal(followers.length, info.followers_count);
+      equal(followers.length, followers_count);
       done();
     });
   })
