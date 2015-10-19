eb68772d1782369279e3526a85b7dcc7a34086a1
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-10-19 14:40:32 +0200

    finally working

--- index.js
+++ index.js
@@ -2,26 +2,17 @@ import Twitter from 'twit';
 import { merge, isEmpty, concat, last } from 'ramda';
 
 function accumulate(get, options, followers, cb) {
-  console.log('accumulate');
   get(options, (err, { users, next_cursor_str: cursor } = res) => {
-    console.log('get');
     if (err) return cb(err);
-    console.log('users.length', users.length);
-    if (cursor === '0') {
-      return cb(null, followers);
-    }
     var accumulatedFollowers = concat(followers, users);
-    var nextOptions = merge(options, { cursor });
-    console.log('accumulatedFollowers length', accumulatedFollowers.length);
-    console.log('last accumulatedFollowers screen_name', last(accumulatedFollowers).screen_name);
-    console.log('new cursor', cursor);
-    console.log('nextOptions cursor', nextOptions.cursor);
-    return accumulate(get, nextOptions, accumulatedFollowers, cb);
+    if (cursor === '0') {
+      return cb(null, accumulatedFollowers);
+    }
+    return accumulate(get, merge(options, { cursor }), accumulatedFollowers, cb);
   });
 }
 
 export default function getTwitterFollowers(tokens, username, cb) {
-  console.log('getTwitterFollowers');
   const client = new Twitter(tokens);
   const get = client.get.bind(client, 'followers/list');
   const options = { screen_name: username, count: 200 };
