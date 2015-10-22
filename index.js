5e69619a799cdf50c86d3e22e3108390d71242e8
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-10-22 17:25:43 +0200

    WIP implmenting users/lookup solution

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 import Twitter from 'twit';
 import { merge, isEmpty, concat, last } from 'ramda';
+import { map, splitEvery, join } from 'ramda';
+
+const splitByHundreds = splitEvery(100);
+const mapRetrieveUserObject = map(retrieveUserObjects);
+const joinWithComma = join(',');
+
+function lookupUserObjects(ids) {
+  const client = new Twitter(tokens);
+  const post = client.post.bind(client, 'users/lookup');
+  const options = { user_id: joinWithComma(ids), include_entities: false };
+  return post(options, (err, users) => {
+    if (err) return cb(err);
+    // TODO: implement with Promises and ramda.composeP
+    resolve(users);
+  });
+}
+
+function retrieveUserObjects(ids) {
+  var splittedIds = splitByHundreds(ids);
+  return splittedIds.map(lookupUserObjects);
+}
 
 function accumulate(get, options, followers, cb) {
   get(options, (err, { ids, next_cursor_str: cursor } = res) => {
