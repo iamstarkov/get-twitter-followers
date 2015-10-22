59aecbaa6404717333b5e29a7b036a29ba927563
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-10-22 16:39:53 +0200

    switched to followers/ids

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import Twitter from 'twit';
 import { merge, isEmpty, concat, last } from 'ramda';
 
 function accumulate(get, options, followers, cb) {
-  get(options, (err, { users, next_cursor_str: cursor } = res) => {
+  get(options, (err, { ids, next_cursor_str: cursor } = res) => {
     if (err) return cb(err);
-    var accumulatedFollowers = concat(followers, users);
+    var accumulatedFollowers = concat(followers, ids);
     if (cursor === '0') {
       return cb(null, accumulatedFollowers);
     }
@@ -14,7 +14,7 @@ function accumulate(get, options, followers, cb) {
 
 export default function getTwitterFollowers(tokens, username, cb) {
   const client = new Twitter(tokens);
-  const get = client.get.bind(client, 'followers/list');
-  const options = { screen_name: username, count: 200 };
+  const get = client.get.bind(client, 'followers/ids');
+  const options = { screen_name: username, stringify_ids: true, count: 5000 };
   return accumulate(get, options, [], cb);
 };
